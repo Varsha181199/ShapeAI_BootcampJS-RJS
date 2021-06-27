808a6c0751b4fdb7631e83198b9c93ca949f3267
Varsha181199 <86563282+Varsha181199@users.noreply.github.com>
2021-06-27 20:39:40 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1>Javascript and React.js</h1>
+      <p>Basic Front end Development with Javascript and React.js-The Foundation of Web Development</p>
+    </div>
+  );
+}
+
+export default Info;
